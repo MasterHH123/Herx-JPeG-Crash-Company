1b31248bb3aa9f748d5e8584012f331d8578a8b4
JuanPglez <79598528+JuanPabloG2@users.noreply.github.com>
2021-04-24 21:11:22 -0500

    3

--- Herx&JPeG.py
+++ Herx&JPeG.py
@@ -1,13 +1,13 @@
+#Se inicia el programa
+#Se le pide al usuario que ingrese su modelo de auto y nombre completos
+
 def startupmenu():
     print("Rellena lo que se te pide para darte un estimado sobre el costo de reparación")
-    input("")
+inicio=input("Ingresa tu modelo de automovil, marca de vehículo y su nombre completo (separado por comas), Por favor: ")
 opcion = int(input("Opción: "))
 print("")
     return opcion
 
-#Se inicia el programa
-#Se le pide al usuario que ingrese su modelo de auto y nombre completos
-inicio=input("Ingresa tu modelo de automovil, marca de vehículo y su nombre completo (separado por comas), Por favor: ")
 #Se imprime una lista de tipos de reparaciones que se ofrecen
 #Se le pide al usuario que introduzca cuantas reparaciones necesita o si su tipo de reparacion no aparece en la lista seleccione "otra"
 #Se le pregunta al usuario si necesita alguna otra reparación
