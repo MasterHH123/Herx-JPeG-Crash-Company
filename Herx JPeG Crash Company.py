30165e2ecae392632dc7ff563cb85a63c3a529ed
JuanPglez <79598528+JuanPabloG2@users.noreply.github.com>
2021-04-24 21:05:53 -0500

    1

--- Herx JPeG Crash Company.py
+++
@@ -1,8 +0,0 @@
-#Se inicia el programa
-#Se le pide al usuario que ingrese su modelo de auto y nombre completos
-inicio=input("Ingresa tu modelo de automovil, marca de vehiculo y su nombre completo (separado por comas), Por favor: ")
-#Se imprime una lista de tipos de reparaciones que se ofrecen
-#Se le pide al usuario que introduzca cuantas reparaciones necesita o si su tipo de reparacion no aparece en la lista seleccione "otra"
-#Se le pregunta al usuario si necesita alguna otra reparacion
-#Sino se imprime el cobro total por las reparaciones requeridas
-print(inicio)
