fec438e105b02a714b6d1106554df74d40921a57
Horacio Hernandez <hernandezh4@hotmail.com>
2021-04-24 19:01:28 -0700

    avance

---
+++ Herx&JPeG.py
@@ -0,0 +1,6 @@
+def startupmenu():
+    print("Rellena lo que se te pide para darte un estimado sobre el cotso de reparación")
+    input("")
+opcion = int(input("Opción: "))
+print("")
+    return opcion
